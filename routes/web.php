11e6ddae2289f440ff547ceb54f6724ec2f5b7eb
DESKTOP-D21AOUO\a-iino <a-iino@orbitalnet.jp>
2021-03-23 14:36:40 +0900

    ユーザー認証セットアップ

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/top/index', function () {
+    return view('top.index');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
